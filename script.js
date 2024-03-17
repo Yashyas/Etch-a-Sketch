474ac25fc9691361fb49bdafb6c1df900df9671d
Yashyas <Ym130602@gmail.com>
2024-03-18 00:57:36 +0530

    Fully working model is created but it is barebones ,more functions need to be added

--- script.js
+++ script.js
@@ -13,6 +13,31 @@ function creation(size){
         row.classList.toggle('row');
         document.querySelector('.box').appendChild(row);
     }
+    traceMouse();
 }
 
+function traceMouse(){
+    let hover =document.querySelectorAll('.column');
+    hover.forEach(function(singleBox){
+    singleBox.addEventListener('mouseenter',color);
+    });
+
+    // 2nd Method to do same thing
+    
+    // let container = document.querySelector('.box');
+    // container.addEventListener('mousemove', function(event){
+    //     if (event.target.classList.contains('column')) {
+    //         color();
+    //         event.target.classList.add('hover');
+    //     }
+    // });
+    
+}
+function color(event){
+    event.target.classList.add('hover');
+    console.log("hovered"+count);
+    count++;
+
+ }
+let count = 0;
 creation(size);
