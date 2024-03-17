5dfbad3e257b4b149790c2e0d1821c8a8282457d
Yashyas <Ym130602@gmail.com>
2024-03-18 01:42:01 +0530

    Added control buttons structure and added clear button

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function creation(size){
     traceMouse();
 }
 
+// mouse tracing function for coloring divs 
 function traceMouse(){
     let hover =document.querySelectorAll('.column');
     hover.forEach(function(singleBox){
@@ -23,7 +24,7 @@ function traceMouse(){
     });
 
     // 2nd Method to do same thing
-    
+
     // let container = document.querySelector('.box');
     // container.addEventListener('mousemove', function(event){
     //     if (event.target.classList.contains('column')) {
@@ -33,11 +34,37 @@ function traceMouse(){
     // });
     
 }
+
+// color of the hovering div 
 function color(event){
     event.target.classList.add('hover');
-    console.log("hovered"+count);
-    count++;
-
  }
-let count = 0;
+
+//  turn on the control buttons event listner 
+ function controlsOn(){
+    let controls = document.querySelectorAll('button');
+    controls.forEach(function(button){
+        button.addEventListener('click',selection);
+    });
+    
+ }
+
+// Selection function for controls 
+ function selection(event){
+    let choice = event.target.id;
+    if(choice=='clear'){
+        clear();
+    }
+ }
+
+//  function to clear all the box selected
+ function clear(){
+    let clear =document.querySelectorAll('.column');
+    clear.forEach(function(singleBox){
+    singleBox.classList.remove('hover');
+    });
+ }
+
+//  main function 
 creation(size);
+controlsOn();
